b26d3ebf030a7220a99c504ee5f78ed8d0b39c4f
IvanDegN <mauzer.bruno@yandex.ru>
2022-08-12 18:03:25 +0300

    Реализовано изменение статуса записи

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -4,14 +4,27 @@ const ToDoList = ({setTodo, todo}) =>
 
     let deleteTodo = (id) =>
     {
-        let newTodo = [...todo].filter(item => item.id !== id) // создание копии массива todo и фильтрация этой копии
+        let newTodo = [...todo].filter(item => item.id !== id) // создание копии массива to-do и фильтрация этой копии
         setTodo(newTodo) // обновление state
     } // функция для удаления записи
+
+    let ChangeStatusTodo =(id) =>
+    {
+        let newTodo = [...todo].filter(item => {
+            if(item.id === id)
+            {
+                item.isClosed = !item.isClosed
+            }
+            return item
+        })
+        setTodo(newTodo);
+    }
 
     let ListItems = todo.map(item => (
         <div key={item.id}>
             <div>{item.title}</div>
-            <button onClick={ ()=>deleteTodo(item.id)  }>Удалить</button>
+            <button onClick={ ()=>deleteTodo(item.id) }>Удалить</button>
+            <button onClick={ ()=>ChangeStatusTodo(item.id)}>Закрыть / Открыть</button>
         </div>
         )
     )
